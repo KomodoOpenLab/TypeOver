17a5f3407eed024bfbeffadd153980f8965a0280
Owen McGirr <o.a.mcgirr@gmail.com>
2013-05-31 13:56:42 +0100

    added VoiceQualityTester project

---
+++ VoiceQualityTester/VoiceQualityTesterTests/VoiceQualityTesterTests.m
@@ -0,0 +1,32 @@
+//
+//  VoiceQualityTesterTests.m
+//  VoiceQualityTesterTests
+//
+//  Created by Owen McGirr on 30/05/2013.
+//  Copyright (c) 2013 Owen McGirr. All rights reserved.
+//
+
+#import "VoiceQualityTesterTests.h"
+
+@implementation VoiceQualityTesterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in VoiceQualityTesterTests");
+}
+
+@end
