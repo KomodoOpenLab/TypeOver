3e3ecadba8bac774c1134ba44b4d02b5b30cd448
Owen McGirr <o.a.mcgirr@gmail.com>
2013-08-13 13:37:48 +0100

    added new custom class for toggle switches

---
+++ TypeOver/TypeOver/CustomToggleSwitch.h
@@ -0,0 +1,13 @@
+//
+//  CustomToggleSwitch.h
+//  TypeOver
+//
+//  Created by Owen McGirr on 12/08/2013.
+//  Copyright (c) 2013 Owen McGirr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomToggleSwitch : UISwitch
+
+@end
