9c5765d0bbb801fea532a6200a1f8cc9d6872a69
Owen McGirr <o.a.mcgirr@gmail.com>
2013-09-02 13:52:00 +0100

    add empty ios 7 mode test project

---
+++ Mockups/iOS7ModeTest/iOS7ModeTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOS7ModeTest
+//
+//  Created by Owen McGirr on 02/09/2013.
+//  Copyright (c) 2013 Owen McGirr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Mockups/iOS7ModeTest/iOS7ModeTestTests/iOS7ModeTestTests.h
@@ -0,0 +1,13 @@
+//
+//  iOS7ModeTestTests.h
+//  iOS7ModeTestTests
+//
+//  Created by Owen McGirr on 02/09/2013.
+//  Copyright (c) 2013 Owen McGirr. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iOS7ModeTestTests : SenTestCase
+
+@end
